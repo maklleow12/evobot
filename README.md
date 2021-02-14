f45c931875bced8b606a68e9674224ebb867dcb2
maklleow12 <70585465+maklleow12@users.noreply.github.com>
2021-02-14 12:09:51 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# evobot
+## Deprecated
+> :warning: 3PG is deprecated. Please use [2PG](https://github.com/twopg) instead. More Info - https://github.com/3PG/Bot/issues/10
+
+# 3PG
+The all-in-one, highly customizable Discord bot.
+
+![Discord](https://img.shields.io/discord/685862664223850497?color=482f5d&amp;label=Support&amp;style=for-the-badge)
+![Lines of Code](https://img.shields.io/tokei/lines/github/3PG/Bot?color=482f5d&style=for-the-badge)
+![Repo Stars](https://img.shields.io/github/stars/3PG/Bot?color=482f5d&style=for-the-badge)
+
+![Dashboard Preview](https://3pg.xyz/assets/docs/img/dashboard-v2.2.0b.png)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/3PG/Bot/tree/stable)
+
+# Non Heroku Setup
+1) Make a .env file in the root project directory.
+2) Fill in the .env
+
+`.env`
+```env
+API_URL="http://localhost:3000/api"
+BOT_ID="533947001578979328"
+BOT_TOKEN="NTI1OTM1MzM1OTE4NjY1NzYw.XB3lcw.z1_F0px-RxqnnJ2ni3Bgn9Fo9aw"
+CLIENT_SECRET="FqRcAkRTJVXM6sdFdJ_EhqNE1sBStKjJ"
+DASHBOARD_URL="http://localhost:4200"
+GUILD_ID="599596068145201152"
+PORT=3000
+PREMIUM_ROLE_ID="598565371162656788"
+OWNER_ID="218459216145285121"
+MONGO_URI="mongodb://localhost/3PG"
+STRIPE_SECRET_KEY="sk_test_yUSJD1JOVcg7WHJmIZjtfLwG00RbMMuCpS"
+STRIPE_WEBHOOK_SECRET=""
+```
---
+++ test/unit/auto-mod-validators.tests.ts
@@ -0,0 +1,101 @@
+import { SavedGuild, GuildDocument, MessageFilter } from '../../src/data/models/guild';
+import { Message } from 'discord.js';
+import { expect } from 'chai';
+import EmojiValidator from '../../src/modules/auto-mod/validators/emoji.validator';
+import MassMentionValidator from '../../src/modules/auto-mod/validators/mass-mention.validator';
+import MassCapsValidator from '../../src/modules/auto-mod/validators/mass-caps.validator';
+import ZalgoValidator from '../../src/modules/auto-mod/validators/zalgo.validator';
+
+describe('auto-mod/validators', () => {
+  let guild: GuildDocument;
+  beforeEach(() => guild = new SavedGuild());
+
+  describe('emoji validator', () => {
+    it('no emojis, does not throw', () => {
+      const validator = new EmojiValidator();
+
+      const result = () => validator.validate('', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('nearly too many emojis, does not throw', () => {
+      const validator = new EmojiValidator();
+
+      const result = () => validator.validate('🤔🤔🤔🤔', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('too many emojis, throws error', () => {
+      const validator = new EmojiValidator();
+
+      const result = () => validator.validate('🤔🤔🤔🤔🤔', guild);
+
+      expect(result).to.throw();
+    });
+  });
+
+  describe('mass mention validator', () => {
+    it('no mentions, does not throw', () => {
+      const validator = new MassMentionValidator();
+
+      const result = () => validator.validate('', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('nearly too many mentions, does not throw', () => {
+      const validator = new MassMentionValidator();
+
+      const result = () => validator.validate('<@!704656805208522833><@!704656805208522833><@!704656805208522833><@!704656805208522833>', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('too many mentions, throws error', () => {
+      const validator = new MassMentionValidator();
+
+      const result = () => validator.validate('<@!704656805208522833><@!704656805208522833><@!704656805208522833><@!704656805208522833><@!704656805208522833>', guild);
+
+      expect(result).to.throw();
+    });
+  });
+
+  describe('all caps validator', () => {
+    it('no caps, does not throw', () => {
+      const validator = new MassCapsValidator();
+
+      const result = () => validator.validate('a', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('nearly too many caps, does not throw', () => {
+      const validator = new MassCapsValidator();
+
+      const result = () => validator.validate('AAAAaaaaaa', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('too many caps, throws error', () => {
+      const validator = new MassCapsValidator();
+
+      const result = () => validator.validate('AAaa', guild);
+
+      expect(result).to.throw();
+    });
+  });
+
+  describe('zalgo validator', () => {
+    it('no zalgo, does not throw', () => {
+      const validator = new ZalgoValidator();
+
+      const result = () => validator.validate('a 🤔', guild);
+
+      expect(result).to.not.throw();
+    });
+    it('zalgo, throws error', () => {
+      const validator = new ZalgoValidator();
+
+      const result = () => validator.validate('a̵͎̟̻̟̺͇̭͚͇̳̔̍͊̈́̀̕͝', guild);
+
+      expect(result).to.throw();
+    });
+  });
+});
